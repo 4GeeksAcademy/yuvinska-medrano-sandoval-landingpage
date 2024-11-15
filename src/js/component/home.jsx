6b259c7b7f3fd365562e8ac57b6ba9c96c77a010
Yuvinska Medrano Sandoval <168204816+yuvinska@users.noreply.github.com>
2024-11-15 21:21:36 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
 import React from "react";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const cards = [
+	{
+		image: "https://picsum.photos/id/10/200/300",
+		title: "Pino",
+		description: "Parque con lindos pinos",
+	},
+	{
+		image: "https://picsum.photos/id/17/200/300",
+		title: "Pasto",
+		description: "lindo paisaje y el pasto de decoracion",
+	},
+	{
+		image: "https://picsum.photos/id/28/200/300",
+		title: "Arbusto",
+		description: "Bosque lleno de arbusto",
+	},
+	{
+		image: "https://picsum.photos/id/54/200/300",
+		title: "Nuves",
+		description: "Montaña con nuves ",
+	}
+];
 
-//create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<div className="container">
+				<div className="row">
+					{cards.map((item, index) => (
+						<div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" key={index}>
+							<Card cards={item} />
+						</div>
+					))}
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 };
